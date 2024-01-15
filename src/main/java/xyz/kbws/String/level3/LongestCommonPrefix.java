aefc6296a20fa6b3022b03dc3fe220c5c141fec0
kbws <hsy040506@163.com>
2024-01-15 18:55:11 +0800

    字符串：字符串冲刺

---
+++ src/main/java/xyz/kbws/String/level3/LongestCommonPrefix.java
@@ -0,0 +1,49 @@
+package xyz.kbws.String.level3;
+
+/**
+ * @author kbws
+ * @date 2024/1/15
+ * @description: <a href="https://leetcode.cn/problems/longest-common-prefix/description/">最长公共前缀</a>
+ */
+public class LongestCommonPrefix {
+    public String longestCommonPrefix1(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        int length = strs[0].length();
+        int count = strs.length;
+        for (int i = 0; i < length; i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < count; j++) {
+                if (i == strs[j].length() || strs[j].charAt(i) != c) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
+
+    public String longestCommonPrefix2(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        String prefix = strs[0];
+        int count = strs.length;
+        for (int i = 1; i < count; i++) {
+            prefix = longestCommonPrefix(prefix, strs[i]);
+            if (prefix.length() == 0) {
+                break;
+            }
+        }
+        return prefix;
+    }
+
+    public String longestCommonPrefix(String str1, String str2) {
+        int length = Math.min(str1.length(), str2.length());
+        int index = 0;
+        while (index < length && str1.charAt(index) == str2.charAt(index)) {
+            index++;
+        }
+        return str1.substring(0, index);
+    }
+}
